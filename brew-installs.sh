84c491a6ff2abf82793a0b475d9665fbabc4caaf
Jørgen Granseth <jorgen.granseth@bekk.no>
2020-01-28 15:22:19 +0100

    Add brew installs script

---
+++ brew-installs.sh
@@ -0,0 +1,9 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install macvim
+brew install zsh
+brew install ctags
+brew install autojump
+brew cask install slack
+brew install node
+brew install maven
